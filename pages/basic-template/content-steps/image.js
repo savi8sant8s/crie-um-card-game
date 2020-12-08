212fa463aa6a324ad2cd0fb87e5d3c6dd7c17316
Sávio Santos <saviosa08@gmail.com>
2020-12-08 10:40:22 -0300

    Finalizado passo a passo de criar card game (piloto).

--- pages/basic-template/content-steps/image.js
+++ pages/basic-template/content-steps/image.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Typography from '@material-ui/core/Typography'
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField'
 
 const style = {
     padding: "1rem",
@@ -7,12 +8,27 @@ const style = {
 
 export default class Image extends Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            file: null
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState({
+            file: URL.createObjectURL(event.target.files[0])
+        })
+    }
+    
     render() {
         return (
             <div style={style}>
                 <Typography variant="h5" color="initial">Agora adicione uma imagem para finalizar</Typography>
-                
+                <TextField type="file" onChange={this.handleChange} variant="outlined" />
+                <img src={this.state.file} />
             </div>
-        )
+        );
     }
 }
